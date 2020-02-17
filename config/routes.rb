f35a1a8be7bd443a8ae045d9fb419743a0bc59ee
Henrique Silva <henrique@dryworks.com.br>
2020-02-17 00:10:42 -0300

    Refactory routes and config locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1,
-  defaults: { format: :json },
-  path: '/' do
+            defaults: { format: :json },
+            path: '/' do
     post '/accounts/transfer', to: 'accounts#transfer', as: :accounts_transfer
     get '/accounts', to: 'accounts#consult', as: :accounts_consult
   end
